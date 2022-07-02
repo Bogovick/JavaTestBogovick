ccc4d880b7649524616a4b313e877fcdd33c9fa8
Bogovick <Sanni25251@gmail.com>
2022-07-03 01:06:31 +0300

    2

--- src/com/company/Example2plus.java
+++ src/com/company/Example2plus.java
@@ -11,7 +11,7 @@ public class Example2plus {
 //    public class Main {
 
         private static int IMAX = 10000; // Maximum value
-        public static int[][] adjMat ={
+        public static int[][] adjMat ={//Initializing an array of messages between cities with their cost
                 {IMAX,1,3,IMAX},
                 {1,IMAX,1,4},
                 {3,1,IMAX,1},
@@ -20,7 +20,7 @@ public class Example2plus {
 
        // public static void main(String[] args) {
        public static void Ex2() {
-           Dijkstra(adjMat, 0, 3);
+           Dijkstra(adjMat, 0, 3);//The first city is counted from 0, the last n-1
            Dijkstra(adjMat, 1, 3);
        }
         //}
@@ -33,17 +33,17 @@ public class Example2plus {
                 isVisted [i] = false; // Point visited
                 d [i] = IMAX; // The shortest path from the starting point to this point among the paths that the current set can connect
             }
-            isVisted[start] = true;
+            isVisted[start] = true;//Visiting the starting point
             d[start] = 0;
             int unVisitNode = martix.length ;
             int index = start;
-            while (unVisitNode > 0 && !isVisted[terminal] ){
+            while (unVisitNode > 0 && !isVisted[terminal] ){//Until the last point visited
                 int min = IMAX;
                 // Choose a point with the shortest path from a point in set A to set B
                 for (int i = 0;i<d.length;i++){
-                    if (min > d[i] && !isVisted[i]){
+                    if (min > d[i] && !isVisted[i]){//If you hit a point that has a connection and it has not yet been visited
                         index = i;
-                        min = d[i];
+                        min = d[i];//Finding the cheapest route
                     }
                 }
 
@@ -53,8 +53,8 @@ public class Example2plus {
                         d[i] = d[index] + martix[index][i];
                 }
 
-                unVisitNode -- ;
-                isVisted[index] = true;
+                unVisitNode -- ;//Each pass we decrease the variable from the length of the array
+                isVisted[index] = true;//Mark visited place
             }
 
             System.out.println(d[terminal]);
